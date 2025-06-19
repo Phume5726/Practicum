aef97816d6794473951027aba1331ea07af37e34
Phume5726 <phumelelamathe49@gmail.com>
2025-06-19 10:17:14 +0200

    First Commit (First in between break for exam)

--- app/src/main/java/vcmsa/ci/practicum/MainActivity.kt
+++ app/src/main/java/vcmsa/ci/practicum/MainActivity.kt
@@ -1,12 +1,21 @@
+//ST10491210
+//Phumelela Langelihle Mathe
 package vcmsa.ci.practicum
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private val song = arrayOf("Song A", "Song B", "Song C", "Song D")
+    private val artist = arrayOf("Artist A", "Artist B", "Artist C", "Artist D")
+    private val rating = intArrayOf(4, 1, 2, 3)
+    private val comments = arrayOf("Rap", "Dance song", "Best love song", "Memories")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,15 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }//code starts here
+
+    var btnExit = findViewById<Button>(R.id.btnExit)
+    var btnNext = findViewById<Button>(R.id.btnNext)
+    var btnAdd = findViewById<Button>(R.id.btnAdd)
+
+    btnExit.setOnClickListener {
+        finish()
     }
+
+
 }
