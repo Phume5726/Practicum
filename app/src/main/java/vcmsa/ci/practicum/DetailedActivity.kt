27351ac492747e26ee312466e06611efc6f8ddc9
Phume5726 <phumelelamathe49@gmail.com>
2025-06-19 11:41:49 +0200

    Second Commit (Completed)

--- app/src/main/java/vcmsa/ci/practicum/DetailedActivity.kt
+++ app/src/main/java/vcmsa/ci/practicum/DetailedActivity.kt
@@ -1,6 +1,8 @@
 package vcmsa.ci.practicum
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,49 @@ class DetailedActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }//code starts here
+        val song = intent.getStringArrayExtra("song")
+        val artist = intent.getStringArrayExtra("artist")
+        val rating = intent.getIntArrayExtra("rating")
+        val comments = intent.getStringArrayExtra("comments")
+
+        val tvDetails = findViewById<TextView>(R.id.tvDetails)
+        val tvAverageRating = findViewById<TextView>(R.id.tvAverageRating)
+        val btnDetails = findViewById<Button>(R.id.btnDetails)
+        val btnCalcAvg = findViewById<Button>(R.id.btnCalcAvg)
+        val btnBack = findViewById<Button>(R.id.btnBack)
+
+        btnDetails.setOnClickListener {
+            var details = "Playlist songs: \n\n"
+            for (i in song!!.indices){
+                details += """
+                    song: ${song[i]}
+                    artist: ${artist!![i]}
+                    rating: ${rating!![i]}
+                    comments: ${comments!![i]}
+                    
+                    """.trimIndent()
+            }
+            tvDetails.text = details
+
         }
+        btnCalcAvg.setOnClickListener {
+            if (rating!!.isNotEmpty()){
+              var sum = 0
+              for (rating in rating){
+                  sum += rating
+              }
+              val average = sum.toDouble() / rating.size
+              tvAverageRating.text = "Average rating: %.2f".format(average)
+                }
+            else {
+                tvAverageRating.text = "No ratings yet"
+            }
+        }
+            btnBack.setOnClickListener {
+                finish()
+
+            }
+        }
+
     }
-}
--- app/src/main/java/vcmsa/ci/practicum/MainActivity.kt
+++ app/src/main/java/vcmsa/ci/practicum/MainActivity.kt
@@ -5,6 +5,7 @@ package vcmsa.ci.practicum
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,24 +17,40 @@ class MainActivity : AppCompatActivity() {
     private val rating = intArrayOf(4, 1, 2, 3)
     private val comments = arrayOf("Rap", "Dance song", "Best love song", "Memories")
 
+    private lateinit var btnExit: Button
+    private lateinit var btnNext: Button
+    private lateinit var btnAdd: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        btnExit = findViewById<Button>(R.id.btnExit)
+        btnNext = findViewById<Button>(R.id.btnNext)
+        btnAdd = findViewById<Button>(R.id.btnAdd)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+        }  btnExit.setOnClickListener {
+                finish()
+           }
+        btnNext.setOnClickListener {
+            val intent = Intent(this@MainActivity, DetailedActivity::class.java)
+
+            intent.putExtra("song", song)
+            intent.putExtra("artist", artist)
+            intent.putExtra("rating", rating)
+            intent.putExtra("comments", comments)
+            startActivity(intent)
         }
-    }//code starts here
+        btnAdd.setOnClickListener {
+            Toast.makeText(this, "Your song has been added to your playlist", Toast.LENGTH_SHORT).show()
+        }
 
-    var btnExit = findViewById<Button>(R.id.btnExit)
-    var btnNext = findViewById<Button>(R.id.btnNext)
-    var btnAdd = findViewById<Button>(R.id.btnAdd)
-
-    btnExit.setOnClickListener {
-        finish()
     }
-
 
 }
